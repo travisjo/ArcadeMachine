8d6e2b4d8e1242dd96774f777e348ea34fe10ebb
Travis <travis.jo@switchninja.com>
2015-11-29 23:17:04 -0600

    collectstatic should work now

--- ArcadeMachine/settings.py
+++ ArcadeMachine/settings.py
@@ -105,6 +105,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
