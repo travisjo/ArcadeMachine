8d10aa30f52badfb109cbbd88c0c4a1b23e9886f
Travis <travis.jo@switchninja.com>
2017-01-16 22:04:15 -0600

    Seperating dev/production credentials in settings

---
+++ ArcadeMachine/local-dist.py
@@ -0,0 +1,14 @@
+# Debug = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'arcademachine',
+        'USER': 'arcademachine',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': 3306,
+    },
+}
