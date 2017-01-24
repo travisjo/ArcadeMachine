2abe69d8fdfdb1aec49e2b60a109f3571d6cdf03
Travis <travis.jo@switchninja.com>
2017-01-23 23:14:23 -0600

    Updated game model and added game fixture

---
+++ base/migrations/0008_auto_20170124_0443.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0007_highscore_photo'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='game',
+            name='year',
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='aliases',
+            field=models.TextField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='giantbomb_id',
+            field=models.IntegerField(null=True),
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='release_date',
+            field=models.DateField(null=True, verbose_name=b'Year released'),
+        ),
+    ]
