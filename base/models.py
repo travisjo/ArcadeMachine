2f57416c3f337974f98d9df306240df4e51de3c4
Travis <travis.jo@switchninja.com>
2017-01-23 23:14:23 -0600

    Updated game model and added game fixture

--- base/models.py
+++ base/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Game(models.Model):
+    giantbomb_id = models.IntegerField(null=True)
     name = models.CharField(max_length=200)
-    year = models.DateField('Year released')
+    aliases = models.TextField(blank=True, null=True)
+    release_date = models.DateField('Year released', null=True)
     latitude = models.DecimalField(max_digits=20, decimal_places=18, null=True, blank=True, default=None)
     longitude = models.DecimalField(max_digits=21, decimal_places=18, null=True, blank=True, default=None)
     date_created = models.DateTimeField('Date created', auto_now_add=True)
