92aca14ca107dbdc25c69de6a6d361579c058e6e
Travis <travis.jo@switchninja.com>
2017-01-24 21:42:17 -0600

    Fixed problem with TempFileUploader. Added S3 support

--- ArcadeMachine/local-dist.py
+++ ArcadeMachine/local-dist.py
@@ -12,3 +12,9 @@ DATABASES = {
         'PORT': 3306,
     },
 }
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = 'arcademachine-images'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_FILE_OVERWRITE = False
